205aa2efe12254080e0cf266c94f5876e77ab89f
Lucas3255 <lucashenrique102006@gmail.com>
2026-05-03 02:47:45 -0300

    ajuste do botao e navbar mobile e estilização.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 const btn = document.querySelector('.menu_toggle');
 const menu = document.querySelector('.list_nav');
+const overlay = document.querySelector('.overlay');
+
+
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
+    overlay.classList.toggle('active');
 });
 
-/*========================================*/
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+/*======================================================*/
 
 const navbar = document.querySelector('.nav_bar');
 
